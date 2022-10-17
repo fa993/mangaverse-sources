03e45d0e28105a9028f618c3f7b285e67806253e
ameyakore <kore.ameya@gmail.com>
2022-10-17 17:54:52 +0530

    cleaner code

--- src/db/genre.rs
+++ src/db/genre.rs
@@ -20,13 +20,13 @@ pub async fn insert_genre(
 
     q.push(" ON DUPLICATE KEY update genre_id = genre_id");
 
-    q.build().execute(&*pool).await?;
+    q.build().execute(pool).await?;
 
     let all = sqlx::query_as!(
         Genre,
         "SELECT genre.genre_id as id, genre.name from genre order by genre.name ASC"
     )
-    .fetch_all(&*pool)
+    .fetch_all(pool)
     .await?;
 
     out.extend(all.into_iter().map(|f| (f.name.to_string(), f)));
